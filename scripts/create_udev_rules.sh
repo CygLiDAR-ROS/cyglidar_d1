0b200e62f26da2464dff6ab2b97b955aa9a35a67
cygbot <45276768+cygbot@users.noreply.github.com>
2021-01-18 13:10:38 +0900

    Update create_udev_rules.sh

--- scripts/create_udev_rules.sh
+++ scripts/create_udev_rules.sh
@@ -3,8 +3,8 @@
 echo "remap the device serial port(ttyUSBX) to  cyglidar"
 echo "ls01c usb cp210x connection as /dev/cyglidar , check it using the command : ls -l /dev|grep ttyUSB"
 echo "start copy cyglidar.rules to  /etc/udev/rules.d/"
-echo "`rospack find cyglidar_pcl`/scripts/cyglidar.rules"
-sudo cp `rospack find cyglidar_pcl`/scripts/cyglidar.rules  /etc/udev/rules.d
+echo "`rospack find cld1a0_pcl`/scripts/cyglidar.rules"
+sudo cp `rospack find cld1a0_pcl`/scripts/cyglidar.rules  /etc/udev/rules.d
 echo " "
 echo "Restarting udev"
 echo ""
