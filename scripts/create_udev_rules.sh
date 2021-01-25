124028ef620a32c18d258f2dffaee1b1761cc18f
cygbot <45276768+cygbot@users.noreply.github.com>
2021-01-25 14:06:24 +0900

    Update create_udev_rules.sh

--- scripts/create_udev_rules.sh
+++ scripts/create_udev_rules.sh
@@ -4,8 +4,8 @@ echo " "
 echo "Remap the device serial port(ttyUSBX) to  cyglidar"
 echo "Check if USB is identified as /dev/cyglidar using the command: ls -l /dev|grep ttyUSB"
 echo "Start copying cyglidar.rules to  /etc/udev/rules.d/"
-echo "`rospack find cld1a0_pcl`/scripts/cyglidar.rules"
-sudo cp `rospack find cld1a0_pcl`/scripts/cyglidar.rules  /etc/udev/rules.d
+echo "`rospack find cyglidar_d1`/scripts/cyglidar.rules"
+sudo cp `rospack find cyglidar_d1`/scripts/cyglidar.rules  /etc/udev/rules.d
 echo " "
 echo "Restart udev"
 echo ""
