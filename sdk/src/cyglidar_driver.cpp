a11322a56c7698f5999c9477a401ce656c11ceca
cygbot <45276768+cygbot@users.noreply.github.com>
2023-04-10 13:04:19 +0900

    Update cyglidar_driver.cpp

--- sdk/src/cyglidar_driver.cpp
+++ sdk/src/cyglidar_driver.cpp
@@ -2,40 +2,45 @@
 #include "serial.h"
 #include "d_series_constant.h"
 
+
 namespace cyg_driver
-{        
+{
     // Convert 2D, 3D rawdata to measured distance appending both separated data (LSB and MSB)
-    void TransformPayload::getDistanceArray2D(uint8_t *payload_data_buffer_2d, int packet_total_length_2d, uint16_t *distance_value_array_buffer_2d)
+    void TransformPayload::getDistanceArray2D(uint8_t *payload_data_buffer_2d, uint16_t *distance_2d)
     {
-        int distance_count_2d = 0;
-
-        for (int data_length = 0; data_length < (packet_total_length_2d - PAYLOAD_SIZE) - 1; data_length += 2)
+        uint8_t distance_count_2d = 0;
+        uint8_t MSB, LSB = 0;
+        uint16_t raw_distance = 0;
+        for (uint16_t data_length = 0; data_length < PACKET_LENGTH_2D - 1; data_length += 2)
         {
-            uint8_t MSB = payload_data_buffer_2d[data_length];
-            uint8_t LSB = payload_data_buffer_2d[data_length + 1];
+            MSB = payload_data_buffer_2d[data_length];
+            LSB = payload_data_buffer_2d[data_length + 1];
 
-            uint16_t raw_distance = ((MSB << 8) | LSB);
+            raw_distance = (uint16_t)((MSB << 8) | LSB);
 
-            distance_value_array_buffer_2d[distance_count_2d++] = raw_distance;
+            distance_2d[distance_count_2d++] = raw_distance;
         }
     }
 
-    void TransformPayload::getDistanceArray3D(uint8_t *payload_data_buffer_3d, int packet_total_length_3d, uint16_t *distance_value_array_buffer_3d)
+    void TransformPayload::getDistanceArray3D(uint8_t *payload_data_buffer_3d, uint16_t *distance_3d)
     {
-        int distance_count_3d = 0;
-        for (int data_length = 0; data_length < (packet_total_length_3d - PAYLOAD_SIZE) - 1; data_length += 3)
+        uint16_t distance_count_3d = 0;
+        uint8_t FIRST, SECOND, THIRD = 0;
+        uint16_t data1, data2 = 0;
+
+        for (uint16_t data_length = 0; data_length < PACKET_LENGTH_3D - 1; data_length += 3)
         {
-            uint8_t FIRST = payload_data_buffer_3d[data_length];
-            uint8_t SECOND = payload_data_buffer_3d[data_length + 1];
-            uint8_t THIRD = payload_data_buffer_3d[data_length + 2];
+            FIRST = payload_data_buffer_3d[data_length];
+            SECOND = payload_data_buffer_3d[data_length + 1];
+            THIRD = payload_data_buffer_3d[data_length + 2];
 
             // data1 is a combination of the first and left half of second
             // data2 is of the right half of second and third
-            uint16_t data1 = (uint16_t)((FIRST << 4) | (SECOND >> 4));
-            uint16_t data2 = (uint16_t)(((SECOND & 0xf) << 8) | THIRD);
+            data1 = (FIRST << 4) | (SECOND >> 4);
+            data2 = ((SECOND & 0xf) << 8) | THIRD;
 
-            distance_value_array_buffer_3d[distance_count_3d++] = data1;
-            distance_value_array_buffer_3d[distance_count_3d++] = data2;
+            distance_3d[distance_count_3d++] = data1;
+            distance_3d[distance_count_3d++] = data2;
         }
     }
 }
