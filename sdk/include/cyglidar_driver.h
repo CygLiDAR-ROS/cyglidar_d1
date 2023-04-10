676a1cf988774582918ad6ad3d60d19b3835d206
cygbot <45276768+cygbot@users.noreply.github.com>
2023-04-10 12:47:54 +0900

    Update cyglidar_driver.h
    For fixing packet data info

--- sdk/include/cyglidar_driver.h
+++ sdk/include/cyglidar_driver.h
@@ -4,11 +4,17 @@
 
 namespace cyg_driver
 {
+    constexpr uint16_t DATA_LENGTH_2D = 161;
+    constexpr uint16_t DATA_LENGTH_3D = 9600;
+
+    constexpr uint16_t PACKET_LENGTH_2D = DATA_LENGTH_2D * sizeof(uint16_t) + 1; //323
+    constexpr uint16_t PACKET_LENGTH_3D = DATA_LENGTH_3D * 1.5 + 1;//14401
+
     class TransformPayload
     {
         public:
-            void getDistanceArray2D(uint8_t *payload_data_buffer_2d, int packet_total_length_2d, uint16_t *distance_value_array_buffer_2d);
-            void getDistanceArray3D(uint8_t *payload_data_buffer_3d, int packet_total_length_3d, uint16_t *distance_value_array_buffer_3d);
+            void getDistanceArray2D(uint8_t *payload_data_buffer_2d, uint16_t *Distance2D);
+            void getDistanceArray3D(uint8_t *payload_data_buffer_3d, uint16_t *Distance3D);
 
         private:
     };
