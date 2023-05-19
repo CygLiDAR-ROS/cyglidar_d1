9a1ada3decaca863875423522046d793aad42679
cygbot <45276768+cygbot@users.noreply.github.com>
2023-05-19 16:41:39 +0900

    Update cyglidar_driver.h
    arrange variables in header file

--- sdk/include/cyglidar_driver.h
+++ sdk/include/cyglidar_driver.h
@@ -1,5 +1,5 @@
-#ifndef CYGLIDAR_DRIVER_H_
-#define CYGLIDAR_DRIVER_H_
+#ifndef __CYGLIDAR_DRIVER_H_
+#define __CYGLIDAR_DRIVER_H_
 #include "stdint.h"
 
 namespace cyg_driver
@@ -8,13 +8,18 @@ namespace cyg_driver
     constexpr uint16_t DATA_LENGTH_3D = 9600;
 
     constexpr uint16_t PACKET_LENGTH_2D = DATA_LENGTH_2D * sizeof(uint16_t) + 1; //323
-    constexpr uint16_t PACKET_LENGTH_3D = DATA_LENGTH_3D * 1.5 + 1;//14401
+    constexpr uint16_t PACKET_LENGTH_3D = DATA_LENGTH_3D * 1.5 + 1;              //14401
 
     class TransformPayload
     {
         public:
-            void getDistanceArray2D(uint8_t *payload_data_buffer_2d, uint16_t *Distance2D);
-            void getDistanceArray3D(uint8_t *payload_data_buffer_3d, uint16_t *Distance3D);
+            uint8_t MSB, LSB;
+            uint16_t raw_distance;
+            void getDistanceArray2D(uint8_t *payload_data_buffer_2d_, uint16_t *distance_2d_);
+
+            uint8_t FIRST, SECOND, THIRD;
+            uint16_t data1, data2;
+            void getDistanceArray3D(uint8_t *payload_data_buffer_3d_, uint16_t *distance_3d_);
 
         private:
     };
