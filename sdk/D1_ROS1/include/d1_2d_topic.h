00ea5a50c20065941aaf7496a449c0e2f084fbf0
cygbot <45276768+cygbot@users.noreply.github.com>
2023-05-21 21:19:39 +0900

    Update d1_2d_topic.h
    some parameters set in header files

--- sdk/D1_ROS1/include/d1_2d_topic.h
+++ sdk/D1_ROS1/include/d1_2d_topic.h
@@ -13,11 +13,13 @@ namespace D1
     class Topic_2D
     {
         public:
-            void publishScanLaser(ros::Publisher publisher_laserscan_, sensor_msgs::LaserScan::Ptr message_laserscan_, std::string frame_id_,
-                                  ros::Time start_, double scan_time_, uint16_t *distance_buffer_2d_);
+            int buffer_index;
+            uint16_t raw_distance;
+            
+            float camera_coordinate_x, camera_coordinate_y;
 
-            void publishPoint2D(ros::Publisher publisher_point_2d_, pcl::PointCloud<pcl::PointXYZRGBA>::Ptr message_point_2d_,
-                                 std::string frame_id_, uint16_t *distance_buffer_2d_);
+            void publishScanLaser(std::string frame_id_, ros::Publisher publisher_laserscan_, ros::Time start_time_, uint16_t *distance_buffer_2d_);
+            void publishPoint2D(std::string frame_id_, ros::Publisher publisher_point_2d_, uint16_t *distance_buffer_2d_);
     };
 }
 
