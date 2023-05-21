df21085f8041c8ebc33d8fa13c858def6dd13850
cygbot <45276768+cygbot@users.noreply.github.com>
2023-05-21 21:09:26 +0900

    Update point_cloud_maker.cpp
    update class function's structure

--- sdk/src/point_cloud_maker.cpp
+++ sdk/src/point_cloud_maker.cpp
@@ -1,60 +1,59 @@
-#include <limits>
-#include <math.h>
 #include "point_cloud_maker.h"
-#include "d_series_camera_lens.h"
-#include "d_series_constant.h"
 
-PointCloudMaker::PointCloudMaker(float *pbuff_x, float *pbuff_y, float *pbuff_z, const int32_t table_total_size_)
+PointCloudMaker::PointCloudMaker(float *point_x_, float *point_y_, float *point_z_, const int32_t table_total_size_)
 {
 	this->table_total_size = table_total_size_;
-	this->table_x = pbuff_x;
-	this->table_y = pbuff_y;
-	this->table_z = pbuff_z;
+	this->table_x = point_x_;
+	this->table_y = point_y_;
+	this->table_z = point_z_;
 }
 
 // centerpoint_offset : change centerpoint by using offset. (unit : pixel)
-void PointCloudMaker::initLensTransform(const float sensor_point_size_MM, const uint32_t width, const uint32_t height, const float centerpoint_offset_X, const float centerpoint_offset_Y)
+void PointCloudMaker::initLensTransform(const float sensor_point_size_mm_, const uint32_t camera_width_, const uint32_t camera_height_,
+										const float center_point_offset_x_, const float center_point_offset_y_)
 {
-	int x, y, row, col;
-	int numCols = width;
-	int numRows = height;
+	number_of_columns = camera_width_;
+	number_of_rows = camera_height_;
 
-	int r0 = 1 - (numRows / 2) + centerpoint_offset_X;
-	int c0 = 1 - (numCols / 2) + centerpoint_offset_Y;
+	int row0 = 1 - (number_of_rows / 2) + center_point_offset_x_;
+	int col0 = 1 - (number_of_columns / 2) + center_point_offset_y_;
 
-	float maxCheck = 0;
-
-	for (y = 0, row = r0; y < numRows; row++, y++)
+	for (y = 0, r = row0; y < number_of_rows; r++, y++)
 	{
-		maxCheck = 0;
-		for (x = 0, col = c0; x < numCols; col++, x++)
+		max_check = 0.0f;
+
+		for (x = 0, c = col0; x < number_of_columns; c++, x++)
 		{
-			float c = static_cast<float>(col) - 0.5f;
-			float r = static_cast<float>(row) - 0.5f;
+			column = static_cast<float>(c) - 0.5f;
+			row = static_cast<float>(r) - 0.5f;
 
-			float angleGrad = getAngle(c, r, sensor_point_size_MM);
-			if (angleGrad > maxCheck) maxCheck = angleGrad;
+			angle_grad = getAngle(column, row, sensor_point_size_mm_);
 
-			float angleRad = angleGrad * 3.14159265f / 180.0f;
+			if (angle_grad > max_check) max_check = angle_grad;
 
-			float rp  = sqrtf((c * c) + (r * r));
-			float rUA = sinf(angleRad);
+			angle_rad = angle_grad * CygLiDARD1::Util::ToRadian;
 
-			table_x[x + (y * CygLiDARD1::Sensor::Width)] = c * rUA / rp;
-			table_y[x + (y * CygLiDARD1::Sensor::Width)] = r * rUA / rp;
-			table_z[x + (y * CygLiDARD1::Sensor::Width)] = cosf(angleRad);
+			float rp  = sqrtf((column * column) + (row * row));
+			float rUA = sinf(angle_rad);
+
+			table_x[x + (y * CygLiDARD1::Sensor::Width)] = column * rUA / rp;
+			table_y[x + (y * CygLiDARD1::Sensor::Width)] = row * rUA / rp;
+			table_z[x + (y * CygLiDARD1::Sensor::Width)] = cosf(angle_rad);
 		}
 	}
 }
 
-eCalculationStatus PointCloudMaker::calcPointCloud(const uint16_t distance, const int32_t index, float& output_x, float& output_y, float& output_z)
+eCalculationStatus PointCloudMaker::calcPointCloud(const uint16_t raw_distance_, const int32_t buffer_index_,
+												   float &point_position_x_, float &point_position_y_, float &point_position_z_)
 {
-	if (index >= table_total_size) return eCalculationStatus::FAIL;
+	if (buffer_index_ >= table_total_size) return eCalculationStatus::FAIL;
 
-	float fDistance = static_cast<float>(distance);
-	output_y = fDistance * table_y[index];
-	output_x = fDistance * table_x[index];
-	output_z = fDistance * table_z[index];
+	float float_distance = static_cast<float>(raw_distance_);
+
+	point_position_x_ = float_distance * table_x[buffer_index_];
+	point_position_y_ = float_distance * table_y[buffer_index_];
+	point_position_z_ = float_distance * table_z[buffer_index_];
+
 	return eCalculationStatus::SUCCESS;
 }
 
@@ -66,21 +65,22 @@ float PointCloudMaker::interpolate(const float x_in, const float x0, const float
 	return ((x_in - x0) * (y1 - y0) / (x1 - x0)) + y0;
 }
 
-float PointCloudMaker::getAngle(const float x, const float y, const float sensor_point_size_MM)
+float PointCloudMaker::getAngle(const float x_, const float y_, const float sensor_point_size_mm_)
 {
-	float radius = sensor_point_size_MM * sqrtf((x * x) + (y * y));
-	float alfaGrad = 0;
+	float radius = sensor_point_size_mm_ * sqrtf((x_ * x_) + (y_ * y_));
+	float alfa_grad = 0;
+
 	for (int i = 1; i < camera_lens_buffer_size; i++)
 	{
 		if (radius >= real_image_height[i - 1] && radius <= real_image_height[i])
 		{
-			alfaGrad = interpolate(radius, real_image_height[i - 1], lens_angle[i - 1], real_image_height[i], lens_angle[i]);
+			alfa_grad = interpolate(radius, real_image_height[i - 1], lens_angle[i - 1], real_image_height[i], lens_angle[i]);
 		}
 	}
-	return alfaGrad;
+	return alfa_grad;
 }
 
-void ColorRGB::initColorMap()
+void PointCloudMaker::initColorMap()
 {
 	uint8_t r_setup = 255;
 	uint8_t g_setup = 0;
@@ -89,7 +89,7 @@ void ColorRGB::initColorMap()
     // Iterate for-loop of adding RGB value to an array
 	for (int i = 0; i < 3; i++)
 	{
-		for (int colorCount = 0; colorCount < 256; colorCount++)
+		for (int color_count = 0; color_count < 256; color_count++)
 		{
 			switch (i)
 			{
