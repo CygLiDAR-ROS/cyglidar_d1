800fac10514bf3b34205cbd419575b68449f4f9a
cygbot <45276768+cygbot@users.noreply.github.com>
2023-05-22 11:30:24 +0900

    Update d1_3d_topic.cpp

--- sdk/D1_ROS2/src/d1_3d_topic.cpp
+++ sdk/D1_ROS2/src/d1_3d_topic.cpp
@@ -1,88 +1,80 @@
-#include "d1_2d_topic.h"
+#include "d1_3d_topic.h"
 
 namespace D1
 {
     using namespace CygLiDARD1;
 
-    void Topic_2D::publishScanLaser(std::string frame_id_, rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr &publisher_laserscan_,
-                                    rclcpp::Time start_time_, uint16_t *distance_buffer_2d_)
+    void Topic_3D::publishScanImage(std::string frame_id_, rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_image_,
+                                    uint16_t *payload_data_buffer_3d)
     {
-        sensor_msgs::msg::LaserScan::SharedPtr message_laserscan(new sensor_msgs::msg::LaserScan);
+        sensor_msgs::msg::Image::SharedPtr message_image(new sensor_msgs::msg::Image);
 
-        message_laserscan->header.frame_id = frame_id_;
-        message_laserscan->header.stamp = start_time_;
-        message_laserscan->angle_min = -static_cast<double>(Sensor::HorizontalAngle / 2.0f * Util::ToRadian);
-        message_laserscan->angle_max =  static_cast<double>(Sensor::HorizontalAngle / 2.0f * Util::ToRadian);
-        message_laserscan->angle_increment = static_cast<double>(Sensor::AngleIncremet2D * Util::ToRadian);
-        message_laserscan->scan_time = 0;
-        message_laserscan->range_min = static_cast<double>(Distance::Mode2D::Minimum_Depth_2D * Util::MM_To_M);
-        message_laserscan->range_max = static_cast<double>(Distance::Mode2D::Maximum_Depth_2D * Util::MM_To_M);
-        message_laserscan->ranges.resize(cyg_driver::DATA_LENGTH_2D);
-        message_laserscan->intensities.resize(cyg_driver::DATA_LENGTH_2D);
+        message_image->header.stamp = rclcpp::Clock().now();
+        message_image->header.frame_id = frame_id_;
+        message_image->height = static_cast<uint32_t>(Sensor::Height);
+        message_image->width  = static_cast<uint32_t>(Sensor::Width);
+        message_image->encoding = sensor_msgs::image_encodings::MONO16;
+        message_image->step = message_image->width * sizeof(uint16_t);
+        message_image->is_bigendian = false;
+        message_image->data.resize(message_image->height * message_image->step);
 
-        for (int i = 0; i < cyg_driver::DATA_LENGTH_2D; i++)
+        depth_data = reinterpret_cast<uint16_t*>(&message_image->data[0]);
+
+        for (int y = 0; y < message_image->height; y++)
         {
-            // Reverse data order of the array
-            buffer_index = (cyg_driver::DATA_LENGTH_2D - 1 - i);
+            for (int x = 0; x < message_image->width; x++)
+            {
+                buffer_index = x + (y * message_image->width);
 
-            if (distance_buffer_2d_[buffer_index] < Distance::Mode2D::Maximum_Depth_2D)
-            {
-                message_laserscan->ranges[i] = distance_buffer_2d_[buffer_index] * MM2M;
-            }
-            else
-            {
-                message_laserscan->ranges[i] = std::numeric_limits<float>::infinity();
+                if (payload_data_buffer_3d[buffer_index] < Distance::Mode3D::Maximum_Depth_3D)
+                {
+                    depth_data[buffer_index] = payload_data_buffer_3d[buffer_index];
+                }
+                else depth_data[buffer_index] = bad_point;
             }
         }
-
-        publisher_laserscan_->publish(*message_laserscan);
+        publisher_image_->publish(*message_image);
     }
 
-    void Topic_2D::publishPoint2D(std::string frame_id_, rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr &publisher_point_2d_,
-                                  uint16_t *distance_buffer_2d_)
+    void Topic_3D::publishPoint3D(std::string frame_id_, rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr &publisher_point_3d_,
+                                  PointCloudMaker &pointcloud_maker, uint16_t *distance_buffer_3d_)
     {
-        pcl::PointCloud<pcl::PointXYZRGBA>::Ptr pointcloud_2d(new pcl::PointCloud<pcl::PointXYZRGBA>);
+        pcl::PointCloud<pcl::PointXYZRGBA>::Ptr pointcloud_3d(new pcl::PointCloud<pcl::PointXYZRGBA>);
 
-        pointcloud_2d->header.frame_id = frame_id_;
-        pointcloud_2d->is_dense = false;
-        pointcloud_2d->points.resize(cyg_driver::DATA_LENGTH_2D);
+        pointcloud_3d->header.frame_id = frame_id_;
+        pointcloud_3d->is_dense = false;
+        pointcloud_3d->width  = Sensor::Width;
+        pointcloud_3d->height = Sensor::Height;
+        pointcloud_3d->points.resize(cyg_driver::DATA_LENGTH_3D);
 
-        angle_increment_steps = static_cast<float>(Sensor::AngleIncremet2D);
-
-        for (int i = 0; i < cyg_driver::DATA_LENGTH_2D; i++)
+        for (buffer_index = 0; buffer_index < Sensor::Height * Sensor::Width; buffer_index++)
         {
-            buffer_index = (cyg_driver::DATA_LENGTH_2D - 1 - i);
+            raw_distance = distance_buffer_3d_[buffer_index];
 
-            raw_distance = distance_buffer_2d_[buffer_index];
-
-            point_2d_angle = ((-HORIZONTAL_ANGLE / 2)+ point_2d_angle_variable) * Util::ToRadian;
-            point_2d_angle_variable += angle_increment_steps;
-
-            camera_coordinate_x = (sin(point_2d_angle) * raw_distance);
-            camera_coordinate_y = (cos(point_2d_angle) * raw_distance);
-
-            pointcloud_2d->points[i].x = camera_coordinate_y * MM2M;
-            pointcloud_2d->points[i].y = camera_coordinate_x * MM2M;
-            pointcloud_2d->points[i].z = 0.0;
-
-            if (distance_buffer_2d_[buffer_index] < Distance::Mode2D::Maximum_Depth_2D)
+            if(raw_distance < Distance::Mode3D::Maximum_Depth_3D)
             {
-                pointcloud_2d->points[i].r = 255;
-                pointcloud_2d->points[i].g = 255;
-                pointcloud_2d->points[i].b = 0;
-                pointcloud_2d->points[i].a = 255;
+                pointcloud_maker.calcPointCloud(raw_distance, buffer_index, camera_coordinate_x, camera_coordinate_y, camera_coordinate_z);
+
+                pointcloud_3d->points[buffer_index].x = camera_coordinate_z * MM2M;
+                pointcloud_3d->points[buffer_index].y = -camera_coordinate_x * MM2M;
+                pointcloud_3d->points[buffer_index].z = -camera_coordinate_y * MM2M;
+                color_change_with_height = pointcloud_maker.color_map[((int)camera_coordinate_y / 2) % pointcloud_maker.color_map.size()];
+                pointcloud_3d->points[buffer_index].rgb = *reinterpret_cast<float *>(&color_change_with_height);
+                pointcloud_3d->points[buffer_index].a = 255;
             }
             else
             {
-                // Turn data invisible when it's greater than the maximum
-                pointcloud_2d->points[i].a = 0;
+                pointcloud_3d->points[buffer_index].x = 0;
+                pointcloud_3d->points[buffer_index].y = 0;
+                pointcloud_3d->points[buffer_index].z = 0;
+                pointcloud_3d->points[buffer_index].rgb = 0;
+                pointcloud_3d->points[buffer_index].a = 0;
             }
         }
+        pcl_conversions::toPCL(rclcpp::Clock().now(), pointcloud_3d->header.stamp);
 
-        pcl_conversions::toPCL(rclcpp::Clock().now(), pointcloud_2d->header.stamp);
-
-        sensor_msgs::msg::PointCloud2 message_point_cloud_2d;
-        pcl::toROSMsg(*pointcloud_2d, message_point_cloud_2d);
-        publisher_point_2d_->publish(message_point_cloud_2d);
+        sensor_msgs::msg::PointCloud2 message_point_cloud_3d;
+        pcl::toROSMsg(*pointcloud_3d, message_point_cloud_3d);
+        publisher_point_3d_->publish(message_point_cloud_3d);
     }
 }
