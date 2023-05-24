d806bc87d9e877015a77d33bebd8ffd632896e73
cygbot <45276768+cygbot@users.noreply.github.com>
2023-05-24 10:40:36 +0900

    Update cyglidar_publisher.cpp

--- sdk/D1_ROS2/cyglidar_publisher.cpp
+++ sdk/D1_ROS2/cyglidar_publisher.cpp
@@ -2,10 +2,9 @@
 
 int main(int argc, char **argv)
 {
-    using namespace D1;
     rclcpp::init(argc, argv);
     rclcpp::executors::SingleThreadedExecutor executor;
-    std::shared_ptr<rclcpp::Node> node = std::make_shared<CygLiDAR_D1>("cyglidar_publisher");
+    std::shared_ptr<rclcpp::Node> node = std::make_shared<D1::CygLiDAR_D1>("cyglidar_publisher");
 
     executor.add_node(node);
     executor.spin();
