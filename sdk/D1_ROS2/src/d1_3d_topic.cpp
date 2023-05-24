b02115e6952076f2a0c1fae2313d393e2840962b
cygbot <45276768+cygbot@users.noreply.github.com>
2023-05-24 10:49:32 +0900

    Update d1_3d_topic.cpp
    do not use pointer type, update point cloud 3d's color map and displaying

--- sdk/D1_ROS2/src/d1_3d_topic.cpp
+++ sdk/D1_ROS2/src/d1_3d_topic.cpp
@@ -37,42 +37,53 @@ namespace D1
     void Topic_3D::publishPoint3D(std::string frame_id_, rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr &publisher_point_3d_,
                                   PointCloudMaker &pointcloud_maker, uint16_t *distance_buffer_3d_)
     {
-        pcl::PointCloud<pcl::PointXYZRGBA>::Ptr pointcloud_3d(new pcl::PointCloud<pcl::PointXYZRGBA>);
+        pcl::PointCloud<pcl::PointXYZRGBA> pointcloud_3d;
 
-        pointcloud_3d->header.frame_id = frame_id_;
-        pointcloud_3d->is_dense = false;
-        pointcloud_3d->width  = Sensor::Width;
-        pointcloud_3d->height = Sensor::Height;
-        pointcloud_3d->points.resize(cyg_driver::DATA_LENGTH_3D);
+        pointcloud_3d.header.frame_id = frame_id_;
+        pointcloud_3d.is_dense = false;
+        pointcloud_3d.width  = Sensor::Width;
+        pointcloud_3d.height = Sensor::Height;
+        pointcloud_3d.points.resize(cyg_driver::DATA_LENGTH_3D);
+
+        asssignPointCloud3DPosition(pointcloud_3d, pointcloud_maker, distance_buffer_3d_);
+
+        pcl_conversions::toPCL(rclcpp::Clock().now(), pointcloud_3d.header.stamp);
+
+        sensor_msgs::msg::PointCloud2 message_point_cloud_3d;
+        pcl::toROSMsg(pointcloud_3d, message_point_cloud_3d);
+        publisher_point_3d_->publish(message_point_cloud_3d);
+    }
+
+    void Topic_3D::asssignPointCloud3DPosition(pcl::PointCloud<pcl::PointXYZRGBA> &pointcloud_3d_, PointCloudMaker &pointcloud_maker_, uint16_t *distance_buffer_3d_)
+    {
+        color_gap = Distance::Mode3D::Maximum_Depth_3D / total_color_number;
 
         for (buffer_index = 0; buffer_index < Sensor::Height * Sensor::Width; buffer_index++)
         {
             raw_distance = distance_buffer_3d_[buffer_index];
 
+            //Color arrangement changes with distance
+            new_color_3d = (int)((float)raw_distance / color_gap) >= total_color_number ? (total_color_number - 1) : (int)raw_distance / color_gap;
+            color_level = total_color_number - new_color_3d;
+
             if(raw_distance < Distance::Mode3D::Maximum_Depth_3D)
             {
-                pointcloud_maker.calcPointCloud(raw_distance, buffer_index, camera_coordinate_x, camera_coordinate_y, camera_coordinate_z);
+                pointcloud_maker_.calcPointCloud(raw_distance, buffer_index, camera_coordinate_x, camera_coordinate_y, camera_coordinate_z);
 
-                pointcloud_3d->points[buffer_index].x = camera_coordinate_z * MM2M;
-                pointcloud_3d->points[buffer_index].y = -camera_coordinate_x * MM2M;
-                pointcloud_3d->points[buffer_index].z = -camera_coordinate_y * MM2M;
-                color_change_with_height = pointcloud_maker.color_map[((int)camera_coordinate_y / 2) % pointcloud_maker.color_map.size()];
-                pointcloud_3d->points[buffer_index].rgb = *reinterpret_cast<float *>(&color_change_with_height);
-                pointcloud_3d->points[buffer_index].a = 255;
+                pointcloud_3d_.points[buffer_index].x = camera_coordinate_z * MM2M;
+                pointcloud_3d_.points[buffer_index].y = -camera_coordinate_x * MM2M;
+                pointcloud_3d_.points[buffer_index].z = -camera_coordinate_y * MM2M;
+                rgb_setup = pointcloud_maker_.color_map[color_level];
+                pointcloud_3d_.points[buffer_index].rgb = *reinterpret_cast<float *>(&rgb_setup);
+                pointcloud_3d_.points[buffer_index].a = 255;
             }
             else
             {
-                pointcloud_3d->points[buffer_index].x = 0;
-                pointcloud_3d->points[buffer_index].y = 0;
-                pointcloud_3d->points[buffer_index].z = 0;
-                pointcloud_3d->points[buffer_index].rgb = 0;
-                pointcloud_3d->points[buffer_index].a = 0;
+                pointcloud_3d_.points[buffer_index].x = 0;
+                pointcloud_3d_.points[buffer_index].y = 0;
+                pointcloud_3d_.points[buffer_index].z = 0;
+                pointcloud_3d_.points[buffer_index].rgba = 0;
             }
         }
-        pcl_conversions::toPCL(rclcpp::Clock().now(), pointcloud_3d->header.stamp);
-
-        sensor_msgs::msg::PointCloud2 message_point_cloud_3d;
-        pcl::toROSMsg(*pointcloud_3d, message_point_cloud_3d);
-        publisher_point_3d_->publish(message_point_cloud_3d);
     }
 }
