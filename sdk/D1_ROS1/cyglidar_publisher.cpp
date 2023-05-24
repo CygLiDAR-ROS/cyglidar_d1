13fce405dc9b54c6783ac89f97c223ec13e99e40
cygbot <45276768+cygbot@users.noreply.github.com>
2023-05-24 10:01:34 +0900

    Update cyglidar_publisher.cpp
    added shutdown() function

--- sdk/D1_ROS1/cyglidar_publisher.cpp
+++ sdk/D1_ROS1/cyglidar_publisher.cpp
@@ -2,12 +2,11 @@
 
 int main(int argc, char **argv)
 {
-    using namespace D1;
     ros::init(argc, argv, "cyglidar_publisher");
     ros::NodeHandle nh;
 
     CygLiDAR_D1 node(nh);
 
     ros::spin();
-    return 0;
+    ros::shutdown();
 }
