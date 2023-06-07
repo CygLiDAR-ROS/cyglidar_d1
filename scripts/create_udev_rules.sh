4eaac473c27760f0259187f75dea9aa34b7200a5
cygbot <45276768+cygbot@users.noreply.github.com>
2023-06-07 16:17:50 +0900

    Update create_udev_rules.sh
    changed package name

--- scripts/create_udev_rules.sh
+++ scripts/create_udev_rules.sh
@@ -4,7 +4,7 @@ echo " "
 echo "remap the device serial port(ttyUSBX) to  cyglidar"
 echo "Check if USB is identified as /dev/cyglidar using the command: ls -l /dev|grep ttyUSB"
 echo "Start copying cyglidar.rules to  /etc/udev/rules.d/"
-sudo cp `ros2 pkg prefix cyglidar_d1`/share/cyglidar_d1/scripts/cyglidar.rules  /etc/udev/rules.d
+sudo cp `ros2 pkg prefix cyglidar_d1_ros2`/share/cyglidar_d1_ros2/scripts/cyglidar.rules  /etc/udev/rules.d
 echo " "
 echo "Restart udev"
 echo ""
