c0a21f5c5002e4b39e10d2ffc243bea73992e162
cygbot <45276768+cygbot@users.noreply.github.com>
2023-07-10 13:50:18 +0900

    Update serial.h
    revised C-type macro

--- sdk/include/serial.h
+++ sdk/include/serial.h
@@ -1,60 +1,18 @@
 #ifndef __SERIAL_H
 #define __SERIAL_H
 
-#include "cygparser.h"
-#include "d_series_constant.h"
-
 #include <boost/asio.hpp>
 #include <cmath>
 #include <thread>
 #include <iostream>
 #include <string>
 
-#define PAYLOAD_SIZE            6
+#include "cygparser.h"
+#include "d_series_constant.h"
 
-#define BASE_DEPTH_2D           10000
-#define BASE_ANGLE_2D           120
-
-#define DISTANCE_MAX_2D         10000
-#define SCAN_MAX_SIZE           20000
-
-#define RIGHT_ANGLE             90
-#define HALF_ANGLE              180
-#define MATH_PI                 3.14159265
-
-#define MM2M                    0.001
-#define MULTIPLY_100            100
-#define FOCAL_LENGTH            40.5
-
-#define HORIZONTAL_ANGLE        120
-#define VERTICAL_ANGLE          75
-
-#define START_COUNT             0
-
-#define BYTESET_NUM_3D          3
-
-#define PACKET_HEADER_0         0
-#define PACKET_HEADER_1         1
-#define PACKET_HEADER_2         2
-#define PAYLOAD_LENGTH_LSB      3
-#define PAYLOAD_LENGTH_MSB      4
-#define PAYLOAD_HEADER          5
-#define PAYLOAD_DATA            6
-
-#define PULSE_LIDAR_TYPE        0
-#define PULSE_CONTROL_MODE      1
-#define PULSE_LIDAR_3D          0
-#define PULSE_LIDAR_DUAL        1
-#define PULSE_AUTO              0
-#define PULSE_MANUAL            1
-
-#define PACKET_HEADER_DEV_INFO  0x10
-#define PACKET_HEADER_2D        0x01
-#define PACKET_HEADER_3D        0x08
-
-#define HEX_SIZE_ONE            4
-#define HEX_SIZE_TWO            8
-#define HEX_SIZE_FOUR           16
+#define SCAN_MAX_SIZE 20000
+#define PULSE_AUTO    0
+#define PULSE_MANUAL  1
 
 enum eRunMode
 {
