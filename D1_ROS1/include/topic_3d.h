87f35518bf252eb26960c17861e4652a7432bdfc
cygbot <45276768+cygbot@users.noreply.github.com>
2023-07-10 14:25:01 +0900

    Update topic_3d.h
    revise C-type macro and change smart pointer type for message

--- D1_ROS1/include/topic_3d.h
+++ D1_ROS1/include/topic_3d.h
@@ -1,17 +1,17 @@
 #ifndef __D1_3D_TOPIC_H
 #define __D1_3D_TOPIC_H
 
-#include "d_series_constant.h"
-#include "cyglidar_driver.h"
-#include "serial.h"
-#include "point_cloud_maker.h"
-
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/image_encodings.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl_ros/point_cloud.h>
+
+#include "d_series_constant.h"
+#include "cyglidar_driver.h"
+#include "serial.h"
+#include "point_cloud_maker.h"
 
 class Topic3D
 {
@@ -31,18 +31,16 @@ class Topic3D
         ros::Publisher publisher_image;
         ros::Publisher publisher_point_3d;
 
-        sensor_msgs::Image message_image;
-        pcl::PointCloud<pcl::PointXYZRGBA> pcl_3d;
+        std::shared_ptr<sensor_msgs::Image> message_image;
+        std::shared_ptr<pcl::PointCloud<pcl::PointXYZRGBA>> pcl_3d;
 
+        const uint16_t bad_point = 0;
         uint16_t  buffer_index;
         uint16_t  raw_distance;
         uint16_t *depth_data;
         uint16_t  color_level;
         uint16_t  total_color_number = 510; //color map array's total size is 510
         uint32_t  rgb_setup;
-
-        const uint16_t bad_point = 0;
-
         float color_gap;
         float camera_coordinate_x, camera_coordinate_y, camera_coordinate_z;
         float param_x[Constant_D1::Sensor::numPixel];
