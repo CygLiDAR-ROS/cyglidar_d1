49a1df41600ca9944daf5fb3aa10486f627e8da5
cygbot <45276768+cygbot@users.noreply.github.com>
2023-08-02 09:59:01 +0900

    Update cyglidar_driver.h

--- sdk/include/cyglidar_driver.h
+++ sdk/include/cyglidar_driver.h
@@ -8,13 +8,18 @@ namespace cyg_driver
     constexpr uint16_t DATA_LENGTH_3D = 9600;
 
     constexpr uint16_t PACKET_LENGTH_2D = DATA_LENGTH_2D * sizeof(uint16_t) + 1; //323
-    constexpr uint16_t PACKET_LENGTH_3D = DATA_LENGTH_3D * 1.5 + 1;//14401
+    constexpr uint16_t PACKET_LENGTH_3D = DATA_LENGTH_3D * 1.5 + 1;              //14401
 
     class TransformPayload
     {
         public:
-            void getDistanceArray2D(uint8_t *payload_data_buffer_2d, uint16_t *Distance2D);
-            void getDistanceArray3D(uint8_t *payload_data_buffer_3d, uint16_t *Distance3D);
+            uint8_t MSB, LSB;
+            uint16_t raw_data;
+            void getDistanceArray2D(uint8_t* _payload_buffer_2d, uint16_t* _distance_2d);
+
+            uint8_t FIRST, SECOND, THIRD;
+            uint16_t data1, data2;
+            void getDistanceArray3D(uint8_t *_payload_buffer_3d, uint16_t* _distance_3d);
 
         private:
     };
