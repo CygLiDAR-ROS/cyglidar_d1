9727da4b77748bf86b2f8279de8d77b436ccd263
cygbot <45276768+cygbot@users.noreply.github.com>
2023-08-02 10:16:04 +0900

    Update cygparser.h
    revise function parameter

--- sdk/include/cygparser.h
+++ sdk/include/cygparser.h
@@ -9,70 +9,43 @@ extern "C" {
 #define DEBUG
 
 // Packet position
-#define POS_CYGBOT_HEADER		0
-#define POS_DEVICE			1
-#define POS_ID				2
-#define POS_LENGTH_1			3
-#define POS_LENGTH_2			4
-#define POS_PAYLOAD_HEADER		5
-
-// Payload position
-#define PAYLOAD_POS_HEADER 		0
-#define PAYLOAD_POS_DATA 		1
+#define CYGBOT_HEADER		0
+#define DEVICE				1
+#define ID					2
+#define LENGTH_LSB			3
+#define LENGTH_MSB			4
+#define PAYLOAD_HEADER		5
+#define PAYLOAD_DATA    	6
 
 // Header1
-#define NORMAL_MODE 			0x5A
-
+#define NORMAL_MODE 		0x5A
 // Header2
-/*
-* ---Code List---
-* BeetleBot = 0x11
-* CygLidar = 0x77
-*/
-#define PRODUCT_CODE 			0x77
-
+#define PRODUCT_CODE 		0x77
 // Header3
 #define DEFAULT_ID 			0xFF
 
-#define CHECKSUM_PASSED 		1
+#define CHECKSUM_PASSED 	1
 
-#define HEADER_LENGTH_SIZE  		5
+enum eCommandMode
+{
+	kIdleMode = 0,
+	kNormalMode
+};
 
-	enum eCommandMode {
-		IdleMode = 0,
-		NormalMode
-	};
+enum ePacketCheckList
+{
+	kHeader1 = 0,
+	kHeader2,
+	kHeader3,
+	kLength_LSB,
+	kLength_MSB,
+	kPayload_Header,
+	kPayload_Data,
+	kCheckSum
+};
 
-	enum ePacketCheckList {
-		Header1 = 0,
-		Header2,
-		Header3,
-		Length1,
-		Length2,
-		Payload_Header,
-		Payload_Data,
-		CheckSum
-	};
-
-	typedef struct CygPayload
-	{
-		uint8_t Header;
-		uint8_t *Data;
-	}CygPayload;
-
-	typedef struct CygPacket
-	{
-		uint8_t Header1;
-		uint8_t Header2;
-		uint8_t Header3;
-		uint16_t Length;
-		CygPayload *Payload;
-		uint8_t Checksum;
-	}Packet;
-
-	uint8_t CygParser(uint8_t *command_buffer, uint8_t packet_data);
-	uint8_t makePacket(uint8_t *command_buffer, Packet *packet, CygPayload *payload);
-	uint8_t calcCheckSum(uint8_t *buffer, int buffer_size);
+uint8_t CygParser(uint8_t* command_buffer, uint8_t packet_data);
+uint8_t calcCheckSum(uint8_t* buffer, int buffer_size);
 
 #ifdef	__cplusplus
 }
