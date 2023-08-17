b1d67cc154bcf348a846ccbe459b9190a8cdc849
cygbot <45276768+cygbot@users.noreply.github.com>
2023-08-17 18:28:59 +0900

    Update topic_3d.h

--- D1_ROS1/include/topic_3d.h
+++ D1_ROS1/include/topic_3d.h
@@ -10,7 +10,6 @@
 
 #include "d_series_constant.h"
 #include "cyglidar_driver.h"
-#include "serial.h"
 #include "point_cloud_maker.h"
 
 class Topic3D
@@ -19,14 +18,14 @@ class Topic3D
         void initPublisher(ros::Publisher _publisher_image, ros::Publisher _publisher_point_3d);
 
         void assignImage(std::string _frame_id);
-        void publishScanImage(uint16_t *_distance_buffer_3d);
+        void publishScanImage(uint16_t* _distance_buffer_3d);
 
         void assignPCL3D(std::string _frame_id);
-        void mappingPointCloud3D(uint16_t *_distance_buffer_3d);
-        void publishPoint3D(uint16_t *_distance_buffer_3d);
+        void mappingPointCloud3D(uint16_t* _distance_buffer_3d);
+        void publishPoint3D(uint16_t* _distance_buffer_3d);
 
     private:
-        PointCloudMaker *pointcloud_maker;
+        PointCloudMaker* pointcloud_maker;
 
         ros::Publisher publisher_image;
         ros::Publisher publisher_point_3d;
@@ -37,7 +36,7 @@ class Topic3D
         const uint16_t bad_point = 0;
         uint16_t  buffer_index;
         uint16_t  raw_distance;
-        uint16_t *depth_data;
+        uint16_t* depth_data;
         uint16_t  color_level;
         uint16_t  total_color_number = 510; //color map array's total size is 510
         uint32_t  rgb_setup;
