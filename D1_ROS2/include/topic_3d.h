53222c206d604a4c9d2045dfb656ee96273844c6
cygbot <45276768+cygbot@users.noreply.github.com>
2023-08-17 17:52:21 +0900

    Update topic_3d.h

--- D1_ROS2/include/topic_3d.h
+++ D1_ROS2/include/topic_3d.h
@@ -1,16 +1,15 @@
 #ifndef __D1_3D_TOPIC_H
 #define __D1_3D_TOPIC_H
 
-#include "d_series_constant.h"
-#include "cyglidar_driver.h"
-#include "serial.h"
-#include "point_cloud_maker.h"
-
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
 #include <sensor_msgs/image_encodings.hpp>
 #include <sensor_msgs/msg/point_cloud2.hpp>
 #include <pcl_conversions/pcl_conversions.h>
+
+#include "d_series_constant.h"
+#include "cyglidar_driver.h"
+#include "point_cloud_maker.h"
 
 using Image = sensor_msgs::msg::Image;
 using PointCloud2 = sensor_msgs::msg::PointCloud2;
@@ -22,11 +21,11 @@ class Topic3D
         void initPublisher(rclcpp::Publisher<Image>::SharedPtr _publisher_image, rclcpp::Publisher<PointCloud2>::SharedPtr _publisher_point_3d);
 
         void assignImage(std::string _frame_id);
-        void publishScanImage(uint16_t *_distance_buffer_3d);
+        void publishScanImage(uint16_t* _distance_buffer_3d);
 
         void assignPCL3D(std::string _frame_id);
-        void mappingPointCloud3D(uint16_t *_distance_buffer_3d);
-        void publishPoint3D(uint16_t *_distance_buffer_3d);
+        void mappingPointCloud3D(uint16_t* _distance_buffer_3d);
+        void publishPoint3D(uint16_t* _distance_buffer_3d);
 
     private:
         PointCloudMaker *pointcloud_maker;
