c45679e37180ec2f539fad9c61d679555fff2c5b
cygbot <45276768+cygbot@users.noreply.github.com>
2023-10-21 20:04:25 +0900

    Update topic_3d.h
    [Update] revise some data type and functions

--- D1_ROS1/include/topic_3d.h
+++ D1_ROS1/include/topic_3d.h
@@ -12,39 +12,45 @@
 #include "cyglidar_driver.h"
 #include "point_cloud_maker.h"
 
+using pcl_XYZRGBA = pcl::PointCloud<pcl::PointXYZRGBA>;
+using namespace Constant_D1;
+
 class Topic3D
 {
     public:
         void initPublisher(ros::Publisher _publisher_image, ros::Publisher _publisher_point_3d);
 
-        void assignImage(std::string _frame_id);
+        void mappingPointCloud3D(uint16_t* _distance_buffer_3d);
+
+        void assignPCL3D(const std::string &_frame_id);
+        void publishPoint3D();
+
+        void assignImage(const std::string &_frame_id);
         void publishScanImage(uint16_t* _distance_buffer_3d);
 
-        void assignPCL3D(std::string _frame_id);
-        void mappingPointCloud3D(uint16_t* _distance_buffer_3d);
-        void publishPoint3D(uint16_t* _distance_buffer_3d);
-
     private:
-        PointCloudMaker* pointcloud_maker;
-
         ros::Publisher publisher_image;
         ros::Publisher publisher_point_3d;
 
-        std::shared_ptr<sensor_msgs::Image> message_image;
-        std::shared_ptr<pcl::PointCloud<pcl::PointXYZRGBA>> pcl_3d;
+        std::shared_ptr<PointCloudMaker>          pointcloud_maker;
+        std::shared_ptr<sensor_msgs::PointCloud2> message_point_cloud_3d;
+        std::shared_ptr<sensor_msgs::Image>       message_image;        
+        std::shared_ptr<pcl_XYZRGBA>              pcl_3d;
 
-        const uint16_t bad_point = 0;
         uint16_t  buffer_index;
         uint16_t  raw_distance;
-        uint16_t* depth_data;
         uint16_t  color_level;
-        uint16_t  total_color_number = 510; //color map array's total size is 510
+        uint16_t  total_color_number = 510;
+        uint16_t* depth_data;
         uint32_t  rgb_setup;
+
         float color_gap;
         float camera_coordinate_x, camera_coordinate_y, camera_coordinate_z;
         float param_x[Constant_D1::Sensor::numPixel];
         float param_y[Constant_D1::Sensor::numPixel];
         float param_z[Constant_D1::Sensor::numPixel];
+
+        const uint16_t BAD_POINT = 0;
 };
 
 #endif
