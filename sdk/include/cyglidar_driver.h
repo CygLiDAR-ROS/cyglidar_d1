5b50839f62992e8f1eb745d08168661329bc631f
cygbot <45276768+cygbot@users.noreply.github.com>
2023-10-21 19:50:40 +0900

    Update cyglidar_driver.h
    [Update] get rid of namespaces and change class name

--- sdk/include/cyglidar_driver.h
+++ sdk/include/cyglidar_driver.h
@@ -1,28 +1,27 @@
-#ifndef CYGLIDAR_DRIVER_H_
-#define CYGLIDAR_DRIVER_H_
-#include "stdint.h"
+#ifndef __CYGLIDAR_DRIVER_H_
+#define __CYGLIDAR_DRIVER_H_
+#include <cstint>
 
-namespace cyg_driver
+constexpr uint16_t DATA_LENGTH_2D = 161;
+constexpr uint16_t DATA_LENGTH_3D = 9600;
+
+constexpr uint16_t PACKET_LENGTH_2D = DATA_LENGTH_2D * sizeof(uint16_t) + 1; //323
+constexpr uint16_t PACKET_LENGTH_3D = DATA_LENGTH_3D * 1.5 + 1;              //14401
+
+class CygDriver
 {
-    constexpr uint16_t DATA_LENGTH_2D = 161;
-    constexpr uint16_t DATA_LENGTH_3D = 9600;
+    public:
+        void getDistanceArray2D(uint8_t* _payload_buffer_2d, uint16_t* _distance_2d);
+        void getDistanceArray3D(uint8_t* _payload_buffer_3d, uint16_t* _distance_3d);
 
-    constexpr uint16_t PACKET_LENGTH_2D = DATA_LENGTH_2D * sizeof(uint16_t) + 1; //323
-    constexpr uint16_t PACKET_LENGTH_3D = DATA_LENGTH_3D * 1.5 + 1;              //14401
+    private:
+        uint8_t buffer_count_2d;
+        uint8_t MSB, LSB;
+        uint16_t raw_data;
 
-    class TransformPayload
-    {
-        public:
-            uint8_t MSB, LSB;
-            uint16_t raw_data;
-            void getDistanceArray2D(uint8_t* _payload_buffer_2d, uint16_t* _distance_2d);
-
-            uint8_t FIRST, SECOND, THIRD;
-            uint16_t data1, data2;
-            void getDistanceArray3D(uint8_t* _payload_buffer_3d, uint16_t* _distance_3d);
-
-        private:
-    };
-}
+        uint16_t buffer_count_3d;
+        uint8_t FIRST, SECOND, THIRD;
+        uint16_t data1, data2;
+};
 
 #endif
