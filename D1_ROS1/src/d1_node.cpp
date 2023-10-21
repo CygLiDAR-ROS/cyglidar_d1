0cdc708fd1efe600547d9290be73a32c70e2b700
cygbot <45276768+cygbot@users.noreply.github.com>
2023-10-21 20:01:16 +0900

    Update d1_node.cpp
    [Update] revise some data type and functions

--- D1_ROS1/src/d1_node.cpp
+++ D1_ROS1/src/d1_node.cpp
@@ -1,14 +1,14 @@
 #include "d1_node.h"
 
-using namespace Constant_D1;
-
-D1_Node::D1_Node(ros::NodeHandle _nh) : nh(_nh)
+D1_Node::D1_Node()
 {
-    topic_2d = new Topic2D();
-    topic_3d = new Topic3D();
+    topic_2d    = std::make_shared<Topic2D>();
+    topic_3d    = std::make_shared<Topic3D>();
+    cyg_driver  = std::make_shared<CygDriver>();
+    serial_port = std::make_shared<CyglidarSerial>();
 
-    topic_2d->initPublisher(nh.advertise<sensor_msgs::LaserScan>  ("scan",    10),
-                            nh.advertise<sensor_msgs::PointCloud2>("scan_2D", 10));
+    topic_2d->initPublisher(nh.advertise<sensor_msgs::LaserScan>  ("scan",       10),
+                            nh.advertise<sensor_msgs::PointCloud2>("scan_2D",    10));
 
     topic_3d->initPublisher(nh.advertise<sensor_msgs::Image>      ("scan_image", 10),
                             nh.advertise<sensor_msgs::PointCloud2>("scan_3D",    10));
@@ -17,54 +17,119 @@ D1_Node::D1_Node(ros::NodeHandle _nh) : nh(_nh)
     received_buffer[1].packet_data = second_total_packet_data;
 
     initConfiguration();
-    
+
     future = exit_signal.get_future();
     double_buffer_thread = std::thread(&D1_Node::doublebufferThread, this);
-    publish_thread = std::thread(&D1_Node::publishThread, this);
+    publish_thread       = std::thread(&D1_Node::publishThread, this);
 }
 
 D1_Node::~D1_Node()
 {
     exit_signal.set_value();
+
     double_buffer_thread.join();
     publish_thread.join();
-    delete topic_2d;
-    delete topic_3d;
-    delete serial_port;
 }
 
-void D1_Node::publishThread()
+void D1_Node::connectBoostSerial()
 {
-    do
+    try
     {
-        runPublish();
-        status = future.wait_for(std::chrono::seconds(0));
-    } while (status == std::future_status::timeout);
-}
+        serial_port->openSerial(port, baud_rate_mode);
 
-void D1_Node::runPublish()
-{
-    if (publish_data_state == PUBLISH_3D)
-    {
-        publish_data_state == PUBLISH_DONE;
-        topic_3d->publishScanImage(distance_buffer_3d);
-        topic_3d->publishPoint3D(distance_buffer_3d);
+        requestPacketData();
     }
-    else if (publish_data_state == PUBLISH_2D)
+    catch (const boost::system::system_error& ex)
     {
-        publish_data_state == PUBLISH_DONE;
-        topic_2d->publishScanLaser(scan_start_time, distance_buffer_2d);
-        topic_2d->publishPoint2D(distance_buffer_2d);
+        ROS_ERROR("[BOOST SERIAL ERROR] %s", ex.what());
     }
 }
 
-void D1_Node::doublebufferThread()
+void D1_Node::disconnectBoostSerial()
 {
-    do
+    serial_port->closeSerial();
+    ROS_ERROR("[PACKET REQUEST] STOP");
+}
+
+void D1_Node::loopCygParser()
+{
+    number_of_data = serial_port->getPacketLength(packet_structure, SCAN_MAX_SIZE);
+
+    for (uint16_t i = 0; i < number_of_data; i++)
     {
-        processDoubleBuffer();
-        status = future.wait_for(std::chrono::seconds(0));
-    } while (status == std::future_status::timeout);
+        parser_return = CygParser(received_buffer[double_buffer_index].packet_data, packet_structure[i]);
+
+        if(parser_return == CHECKSUM_PASSED)
+        {
+            publish_done_flag |= (1 << double_buffer_index);
+
+            double_buffer_index++;
+            double_buffer_index &= 1;
+
+            convertInfoData(&received_buffer[0]);
+        }
+    }
+}
+
+void D1_Node::initConfiguration()
+{
+    ros::NodeHandle priv_nh("~");
+    priv_nh.param<std::string>("port",              port,              "/dev/ttyUSB0");
+    priv_nh.param<int>        ("baud_rate",         baud_rate_mode,    0);
+    priv_nh.param<std::string>("frame_id",          frame_id,          "laser_frame");
+    priv_nh.param<int>        ("run_mode",          run_mode,          2);
+    priv_nh.param<int>        ("duration_mode",     duration_mode,     PULSE_AUTO);
+    priv_nh.param<int>        ("duration_value",    duration_value,    10000);
+    priv_nh.param<int>        ("frequency_channel", frequency_channel, 0);
+
+    topic_2d->assignLaserScan(frame_id);
+    topic_2d->assignPCL2D(frame_id);
+    topic_3d->assignImage(frame_id);
+    topic_3d->assignPCL3D(frame_id);
+}
+
+void D1_Node::requestPacketData()
+{
+    serial_port->requestDeviceInfo();
+    ros::Duration(3.0).sleep();
+    // sleep for 3s, by requsting the info data.
+
+    serial_port->requestRunMode(static_cast<eRunMode>(run_mode), mode_notice);
+    ROS_INFO("[PACKET REQUEST] %s", mode_notice.c_str());
+
+    serial_port->requestDurationControl(static_cast<eRunMode>(run_mode), duration_mode, duration_value);
+    ros::Duration(1.0).sleep();
+    ROS_INFO("[PACKET REQUEST] PULSE DURATION : %d", duration_value);
+    // sleep for a sec, by requsting the duration
+
+    serial_port->requestFrequencyChannel(frequency_channel);
+    ROS_INFO("[PACKET REQUEST] FREQUENCY CH.%d", frequency_channel);
+}
+
+void D1_Node::convertData(received_data_buffer* _received_buffer)
+{
+    if (_received_buffer->packet_data[PAYLOAD_HEADER] == PACKET_HEADER_2D)
+    {
+        scan_start_time = ros::Time::now() - ros::Duration(0.00048);
+
+        cyg_driver->getDistanceArray2D(&_received_buffer->packet_data[PAYLOAD_DATA], distance_buffer_2d);
+        publish_data_state = PUBLISH_2D;
+    }
+    else if (_received_buffer->packet_data[PAYLOAD_HEADER] == PACKET_HEADER_3D)
+    {
+        cyg_driver->getDistanceArray3D(&_received_buffer->packet_data[PAYLOAD_DATA], distance_buffer_3d);
+        publish_data_state = PUBLISH_3D;
+    }
+}
+
+void D1_Node::convertInfoData(received_data_buffer* _received_buffer)
+{
+    if (_received_buffer->packet_data[PAYLOAD_HEADER] == PACKET_HEADER_DEV_INFO && info_flag == false)
+    {
+        ROS_INFO("[F/W VERSION] %d.%d.%d", _received_buffer->packet_data[6], _received_buffer->packet_data[7],  _received_buffer->packet_data[8]);
+        ROS_INFO("[H/W VERSION] %d.%d.%d", _received_buffer->packet_data[9], _received_buffer->packet_data[10], _received_buffer->packet_data[11]);
+        info_flag = true;
+    }
 }
 
 void D1_Node::processDoubleBuffer()
@@ -81,99 +146,38 @@ void D1_Node::processDoubleBuffer()
     }
 }
 
-void D1_Node::initConfiguration()
+void D1_Node::runPublish()
 {
-    ros::NodeHandle priv_nh("~");
-    priv_nh.param<std::string>("port", port, "/dev/ttyUSB0");
-    priv_nh.param<int>("baud_rate", baud_rate, 3000000);
-    priv_nh.param<std::string>("frame_id", frame_id, "laser_frame");
-    priv_nh.param<int>("run_mode", run_mode, 2);
-    priv_nh.param<int>("duration_mode", duration_mode, PULSE_AUTO);
-    priv_nh.param<int>("duration_value", duration_value, 10000);
-    priv_nh.param<int>("frequency_channel", frequency_channel, 0);
-
-    topic_2d->assignLaserScan(frame_id);
-    topic_2d->assignPCL2D(frame_id);
-    topic_3d->assignImage(frame_id);
-    topic_3d->assignPCL3D(frame_id);
-}
-
-void D1_Node::connectBoostSerial()
-{
-    try
+    if (publish_data_state == PUBLISH_3D)
     {
-        // Open the port
-        serial_port = new cyglidar_serial(port, baud_rate, io_service);
-
-        requestPacketData();
+        publish_data_state = PUBLISH_DONE;
+        topic_3d->mappingPointCloud3D(distance_buffer_3d);
+        topic_3d->publishPoint3D();
+        topic_3d->publishScanImage(distance_buffer_3d);
     }
-    catch (const boost::system::system_error& ex)
+    else if (publish_data_state == PUBLISH_2D)
     {
-        ROS_ERROR("[BOOST SERIAL ERROR] %s", ex.what());
+        publish_data_state = PUBLISH_DONE;
+        topic_2d->mappingPointCloud2D(distance_buffer_2d);
+        topic_2d->publishPoint2D();
+        topic_2d->publishScanLaser(scan_start_time, distance_buffer_2d);
     }
 }
 
-void D1_Node::disconnectBoostSerial()
+void D1_Node::doublebufferThread()
 {
-    serial_port->close();
-    ROS_ERROR("[PACKET REQUEST] STOP");
+    do
+    {
+        processDoubleBuffer();
+        status = future.wait_for(std::chrono::seconds(0));
+    } while (status == std::future_status::timeout);
 }
 
-void D1_Node::loopCygParser()
+void D1_Node::publishThread()
 {
-    number_of_data = serial_port->getPacketLength(packet_structure, SCAN_MAX_SIZE);
-
-    for (int i = 0; i < number_of_data; i++)
+    do
     {
-        parser_return = CygParser(received_buffer[double_buffer_index].packet_data, packet_structure[i]);
-
-        if(parser_return == CHECKSUM_PASSED)
-        {
-            publish_done_flag |= (1 << double_buffer_index);
-
-            double_buffer_index++;
-            double_buffer_index &= 1;
-
-            if (received_buffer->packet_data[PAYLOAD_HEADER] == PACKET_HEADER_DEV_INFO && info_flag == false)
-            {
-                ROS_INFO("[F/W VERSION] %d.%d.%d", received_buffer->packet_data[6], received_buffer->packet_data[7], received_buffer->packet_data[8]);
-                ROS_INFO("[H/W VERSION] %d.%d.%d", received_buffer->packet_data[9], received_buffer->packet_data[10], received_buffer->packet_data[11]);
-                info_flag = true;
-            }
-        }
-    }
-}
-
-void D1_Node::convertData(received_data_buffer* _received_buffer)
-{
-    if (_received_buffer->packet_data[PAYLOAD_HEADER] == PACKET_HEADER_2D)
-    {
-        scan_start_time = ros::Time::now() - ros::Duration(0.00048);
-
-        TransformPayload.getDistanceArray2D(&_received_buffer->packet_data[PAYLOAD_DATA], distance_buffer_2d);
-        publish_data_state = PUBLISH_2D;
-    }
-    else if (_received_buffer->packet_data[PAYLOAD_HEADER] == PACKET_HEADER_3D)
-    {
-        TransformPayload.getDistanceArray3D(&_received_buffer->packet_data[PAYLOAD_DATA], distance_buffer_3d);
-        publish_data_state = PUBLISH_3D;
-    }
-}
-
-void D1_Node::requestPacketData()
-{
-    serial_port->requestDeviceInfo();
-    ros::Duration(3.0).sleep();
-    // sleep for 3s, by requsting the info data.
-
-    serial_port->requestRunMode(static_cast<eRunMode>(run_mode), mode_notice);
-    ROS_INFO("[PACKET REQUEST] %s", mode_notice.c_str());
-
-    serial_port->requestDurationControl(static_cast<eRunMode>(run_mode), duration_mode, duration_value);
-    ros::Duration(1.0).sleep();
-    ROS_INFO("[PACKET REQUEST] PULSE DURATION : %d", duration_value);
-    // sleep for a sec, by the duration
-
-    serial_port->requestFrequencyChannel(frequency_channel);
-    ROS_INFO("[PACKET REQUEST] FREQUENCY CH.%d", frequency_channel);
+        runPublish();
+        status = future.wait_for(std::chrono::seconds(0));
+    } while (status == std::future_status::timeout);
 }
