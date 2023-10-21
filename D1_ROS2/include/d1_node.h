c556aaf3d01dbcd856be4e09163a8483349a57d9
cygbot <45276768+cygbot@users.noreply.github.com>
2023-10-21 20:54:36 +0900

    Update d1_node.h
    [Update] Added 'rclcpp::Node' inheritance and revised some functions and declaration

--- D1_ROS2/include/d1_node.h
+++ D1_ROS2/include/d1_node.h
@@ -7,14 +7,16 @@
 #include "cyglidar_serial.h"
 #include "cyglidar_driver.h"
 #include "d_series_constant.h"
-#include "point_cloud_maker.h"
 #include "topic_2d.h"
 #include "topic_3d.h"
 
-class D1_Node
+using namespace std::chrono_literals;
+using namespace Constant_D1;
+
+class D1_Node : public rclcpp::Node
 {
     public:
-        explicit D1_Node(rclcpp::Node::SharedPtr _node);
+        explicit D1_Node();
         virtual ~D1_Node();
 
         void connectBoostSerial();
@@ -30,28 +32,29 @@ class D1_Node
         void initConfiguration();
         void requestPacketData();
         void convertData(received_data_buffer* _received_buffer);
+        void convertInfoData(received_data_buffer* _received_buffer);
         void processDoubleBuffer();
         void runPublish();
         void doublebufferThread();
         void publishThread();
 
-        Topic2D* topic_2d;
-        Topic3D* topic_3d;
-        cyglidar_serial* serial_port;
-        cyg_driver::TransformPayload TransformPayload;
+        std::shared_ptr<Topic2D>        topic_2d;
+        std::shared_ptr<Topic3D>        topic_3d;
+        std::shared_ptr<CyglidarSerial> serial_port;
+        std::shared_ptr<CygDriver>      cyg_driver;
 
         std::string port;
-        int baud_rate;
+        uint8_t baud_rate_mode;
         std::string frame_id;
-        int run_mode;
-        int duration_mode;
-        int duration_value;
-        int frequency_channel;
+        uint8_t run_mode;
+        uint8_t duration_mode;
+        uint16_t duration_value;
+        uint8_t frequency_channel;
+        uint8_t color_mode;
+        uint16_t min_resolution;
+        uint16_t max_resolution;
 
-        rclcpp::Node::SharedPtr node;
         rclcpp::Time scan_start_time;
-
-        boost::asio::io_service io_service;
 
         std::thread double_buffer_thread;
         std::thread publish_thread;
@@ -61,8 +64,8 @@ class D1_Node
         std::future_status status;
 
         std::string mode_notice;
+        bool info_flag = false;
 
-        uint8_t info_flag = 0;
         uint8_t publish_done_flag  = 0;
         uint8_t publish_data_state = 0;
         uint8_t double_buffer_index;
@@ -72,8 +75,8 @@ class D1_Node
         uint8_t parser_return;
 
         uint16_t number_of_data;
-        uint16_t distance_buffer_2d[cyg_driver::DATA_LENGTH_2D];
-        uint16_t distance_buffer_3d[cyg_driver::DATA_LENGTH_3D];
+        uint16_t distance_buffer_2d[DATA_LENGTH_2D];
+        uint16_t distance_buffer_3d[DATA_LENGTH_3D];
 
         const uint8_t PUBLISH_DONE = 0;
         const uint8_t PUBLISH_2D   = 1;
