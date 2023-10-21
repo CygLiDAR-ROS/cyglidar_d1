1d3eb75a98225970a39394cdc8e0ac140759a0e4
cygbot <45276768+cygbot@users.noreply.github.com>
2023-10-21 19:52:10 +0900

    Update cyglidar_serial.h
    [Update] change class name and revise 'serial_port' data type

--- sdk/include/cyglidar_serial.h
+++ sdk/include/cyglidar_serial.h
@@ -13,6 +13,8 @@
 #define PULSE_AUTO    0
 #define PULSE_MANUAL  1
 
+using namespace Constant_D1::Command;
+
 enum eRunMode
 {
     Mode2D,
@@ -20,21 +22,25 @@ enum eRunMode
     ModeDual
 };
 
-class cyglidar_serial
+class CyglidarSerial
 {
     public:
         /**
-            * @brief Construct CygLiDAR attached to the given serial port
-            * @param port_ The string for the serial port device to attempt to connect to, e.g. "/dev/ttyUSB0"
-            * @param baud_rate_ The baud rate to open the serial port at
-            * @param io_ Boost ASIO IO Service to use when creating the serial port object
+            * @brief Default constructor
             */
-        cyglidar_serial(const std::string &_port, uint32_t _baudrate, boost::asio::io_service &_io_service);
+        CyglidarSerial();
 
         /**
             * @brief Default destructor
             */
-        ~cyglidar_serial();
+        virtual ~CyglidarSerial();
+
+        /**
+            * @brief Construct CygLiDAR attached to the given serial port
+            * @param _port The string for the serial port device to attempt to connect to, e.g. "/dev/ttyUSB0"
+            * @param _baudrate The baud rate to open the serial port at
+            */
+        void openSerial(const std::string &_port, const uint8_t _baudrate);
 
         /**
             * @brief Poll the laser to get a new scan. Block until a complete new scan is received or close is called.
@@ -49,7 +55,7 @@ class cyglidar_serial
         /**
             * @brief Send a packet to change a width of the pulse
             */
-        void requestDurationControl(const eRunMode _run_mode, uint8_t _duration_mode, uint16_t _duration_value);
+        void requestDurationControl(const eRunMode _run_mode, const uint8_t _duration_mode, const uint16_t _duration_value);
 
         /**
             * @brief Send a packet to assign a frequency level
@@ -67,19 +73,26 @@ class cyglidar_serial
         void requestSerialBaudRate(const uint8_t _select_baud_rate);
 
         /**
+            * @brief Get a value to set baud rate
+            */
+        uint32_t getBaudRate(uint8_t _baud_rate_mode);
+
+        /**
             * @brief Close the driver down and prevent the polling loop from advancing
             */
-        void close();
+        void closeSerial();
 
     private:
         void sendCommand(const std::vector<uint8_t> &payload);
 
-        boost::asio::serial_port serial; ///< @brief Actual serial port object for reading/writing to the lidar Scanner
+        std::shared_ptr<boost::asio::serial_port> serial; ///< @brief Actual serial port object for reading/writing to the lidar Scanner
+        boost::asio::io_service io_service;
         boost::system::error_code error_code;
 
-        uint8_t payload_length_LSB;
-        uint8_t payload_length_MSB;
+        uint8_t  payload_length_LSB;
+        uint8_t  payload_length_MSB;
         uint16_t number_of_packet;
+        uint32_t baud_rate;
 
         std::vector<uint8_t> command_buffer;
         std::vector<uint8_t> payload_buffer;
