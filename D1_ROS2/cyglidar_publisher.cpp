4cfbeb7921cc43e7c81fbcef30630ef774294dc2
cygbot <45276768+cygbot@users.noreply.github.com>
2023-10-21 20:53:26 +0900

    Update cyglidar_publisher.cpp
    [Update] Changed 'D1_Node' class declaration

--- D1_ROS2/cyglidar_publisher.cpp
+++ D1_ROS2/cyglidar_publisher.cpp
@@ -1,30 +1,26 @@
 #include "d1_node.h"
 
-D1_Node *D1_node;
-
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
-    std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("D1_NODE");
 
-    D1_node = new D1_Node(node);
+    std::shared_ptr<D1_Node> d1_node = std::make_shared<D1_Node>();
 
     try
-    {    
-        D1_node->connectBoostSerial();
+    {
+        d1_node->connectBoostSerial();
 
         while(rclcpp::ok())
         {
-            D1_node->loopCygParser();
+            d1_node->loopCygParser();
         }
 
-        D1_node->disconnectBoostSerial();
+        d1_node->disconnectBoostSerial();
     }
     catch (const rclcpp::exceptions::RCLError &e)
     {
-        RCLCPP_ERROR(node->get_logger(), "[D1 NODE ERROR] : %s", e.what());
+        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "[D1 NODE ERROR] : %s", e.what());
     }
 
     rclcpp::shutdown();
-    delete D1_node;
 }
