7264d20a6cac62d937643e08c05318541de58265
cygbot <45276768+cygbot@users.noreply.github.com>
2023-10-21 20:02:53 +0900

    Update topic_2d.h
    [Update] revise some data type and functions / Added pointCloud2 message

--- D1_ROS1/include/topic_2d.h
+++ D1_ROS1/include/topic_2d.h
@@ -10,28 +10,35 @@
 #include "d_series_constant.h"
 #include "cyglidar_driver.h"
 
+using pcl_XYZRGBA = pcl::PointCloud<pcl::PointXYZRGBA>;
+
 class Topic2D
 {
     public:
         void initPublisher(ros::Publisher _publisher_laserscan, ros::Publisher _publisher_point_2d);
-
-        void assignLaserScan(std::string _frame_id);
-        void publishScanLaser(ros::Time _start_time, uint16_t* _distance_buffer_2d);
-
-        void assignPCL2D(std::string _frame_id);
+        
         void mappingPointCloud2D(uint16_t* _distance_buffer_2d);
-        void publishPoint2D(uint16_t* _distance_buffer_2d);
+
+        void assignPCL2D(const std::string &_frame_id);
+        void publishPoint2D();
+
+        void assignLaserScan(const std::string &_frame_id);
+        void publishScanLaser(ros::Time _start_time, uint16_t* _distance_buffer_2d);
 
     private:
         ros::Publisher publisher_laserscan;
         ros::Publisher publisher_point_2d;
-        std::shared_ptr<sensor_msgs::LaserScan> message_laserscan;
-        std::shared_ptr<pcl::PointCloud<pcl::PointXYZRGBA>> pcl_2d;
+        std::shared_ptr<sensor_msgs::LaserScan>   message_laserscan;
+        std::shared_ptr<sensor_msgs::PointCloud2> message_point_cloud_2d;
+        std::shared_ptr<pcl_XYZRGBA>              pcl_2d;
 
         uint8_t buffer_index;
         uint16_t raw_distance;
+        float point_2d_angle;
+        float point_2d_angle_variable;
         float camera_coordinate_x, camera_coordinate_y;
-        const float angle_increment_steps = Constant_D1::Sensor::AngleIncremet2D;
+
+        const float ANGLE_INCREMENT_STEPS = Constant_D1::Sensor::AngleIncremet2D;
         const float HORIZONTAL_ANGLE = 120.0f;
 };
 
